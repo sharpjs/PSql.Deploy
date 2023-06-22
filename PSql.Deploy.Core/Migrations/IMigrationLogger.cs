0e571fdf95bcc9057edb3e46357480d52821fa33
Jeff Sharp <public@sharpjs.net>
2023-06-22 09:58:41 -0700

    Add IMigrationLogger.

---
+++ PSql.Deploy.Core/Migrations/IMigrationLogger.cs
@@ -0,0 +1,11 @@
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
+
+namespace PSql.Deploy.Migrations;
+
+/// <summary>
+///   An object that logs messages produced during migration.
+/// </summary>
+public interface IMigrationLogger : ISqlMessageLogger
+{
+}
