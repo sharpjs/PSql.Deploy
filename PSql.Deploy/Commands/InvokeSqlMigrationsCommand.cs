324af870feaa0b4077f17ed727bb825cd47f193c
Jeff Sharp <public@sharpjs.net>
2023-06-23 13:44:49 -0700

    Implement IMigrationLogger in Invoke-SqlMigrations command.

--- PSql.Deploy/Commands/InvokeSqlMigrationsCommand.cs
+++ PSql.Deploy/Commands/InvokeSqlMigrationsCommand.cs
@@ -1,12 +1,14 @@
 // Copyright 2023 Subatomix Research Inc.
 // SPDX-License-Identifier: ISC
 
+using PSql.Deploy.Migrations;
+
 namespace PSql.Deploy.Commands;
 
 // TODO: Replace original Invoke-SqlMigrations with this, removing the '2' suffix
 //                                           V
 [Cmdlet(VerbsLifecycle.Invoke, "SqlMigrations2")]
-public class InvokeSqlMigrationsCommand : Cmdlet
+public class InvokeSqlMigrationsCommand : Cmdlet, IMigrationLogger
 {
     // -Path
     [Parameter(Mandatory = true, Position = 0)]
@@ -21,5 +23,10 @@ public class InvokeSqlMigrationsCommand : Cmdlet
 
     protected override void BeginProcessing()
     {
+    }
+
+    void IMigrationLogger.Log(MigrationMessage message)
+    {
+        WriteHost(message.ToString()!);
     }
 }
