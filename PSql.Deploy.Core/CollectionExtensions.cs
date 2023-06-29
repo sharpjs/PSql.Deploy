2082a7cf8926e8b7132a619393ac68b5b47739aa
Jeff Sharp <public@sharpjs.net>
2023-06-29 09:19:44 -0700

    Add extension method to use best build method for ImmutableArray.

---
+++ PSql.Deploy.Core/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
+
+using System.Collections.Immutable;
+
+namespace PSql.Deploy;
+
+internal static class CollectionExtensions
+{
+    public static ImmutableArray<T> Build<T>(this ImmutableArray<T>.Builder builder)
+    {
+        if (builder is null)
+            throw new ArgumentNullException(nameof(builder));
+
+        return builder.Count == builder.Capacity
+            ? builder.MoveToImmutable()
+            : builder.    ToImmutable();
+    }
+}
