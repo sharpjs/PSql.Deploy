9b6fab2df5bd41989a7772378af2e2e9a053ee7b
Jeff Sharp <public@sharpjs.net>
2023-06-29 10:51:55 -0700

    Add GetItems method.

--- PSql.Deploy.Core/Migrations/MigrationPlan.cs
+++ PSql.Deploy.Core/Migrations/MigrationPlan.cs
@@ -17,7 +17,7 @@ internal class MigrationPlan
     /// <summary>
     ///   Gets the migrations to run during the <c>Core</c> phase.
     /// </summary>
-    public List<(Migration, MigrationPhase)> Core { get; } = new();
+    public List<(Migration Migration, MigrationPhase Phase)> Core { get; } = new();
 
     /// <summary>
     ///   Gets the migrations whose <c>Post</c> components actually will run
@@ -29,4 +29,24 @@ internal class MigrationPlan
     ///   Gets whether the migration plan requires downtime.
     /// </summary>
     public bool RequiresDowntime => Core.Count > 0;
+
+    /// <summary>
+    ///   Gets the sequence of items to run in the specified phase.
+    /// </summary>
+    /// <param name="phase">
+    ///   The phase for which to get items to run.
+    /// </param>
+    /// <returns>
+    ///   The sequence of items to run in <paramref name="phase"/>.
+    /// </returns>
+    public IEnumerable<(Migration Migration, MigrationPhase Phase)> GetItems(MigrationPhase phase)
+    {
+        return phase switch
+        {
+            MigrationPhase.Pre  => Pre .Select(m => (m, phase)),
+            MigrationPhase.Core => Core,
+            MigrationPhase.Post => Post.Select(m => (m, phase)),
+            _ => throw new ArgumentOutOfRangeException(nameof(phase)),
+        };
+    }
 }
