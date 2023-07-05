92b31cc88f8e85997cf7f63b9933b8c1ef6f84bb
Jeff Sharp <public@sharpjs.net>
2023-07-05 15:44:01 -0700

    Add MathEx and its tests.

---
+++ PSql.Deploy.Tests/Utilities/MathExTests.cs
@@ -0,0 +1,26 @@
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
+
+namespace PSql.Deploy;
+
+[TestFixture]
+public class MathExTests
+{
+    [Test]
+    [TestCase(-0x8000_0000,           1)]
+    [TestCase(           0,           1)]
+    [TestCase(           1,           1)]
+    [TestCase(           2,           2)]
+    [TestCase(           3,           4)]
+    [TestCase(           4,           4)]
+    [TestCase(           5,           8)]
+    [TestCase( 0x3FFF_FFFF, 0x4000_0000)]
+    [TestCase( 0x4000_0000, 0x4000_0000)]
+    [TestCase( 0x4000_0001, 0x7FFF_FFFF)]
+    [TestCase( 0x7FFF_FFFE, 0x7FFF_FFFF)]
+    [TestCase( 0x7FFF_FFFF, 0x7FFF_FFFF)]
+    public void GetNextPowerOf2Saturating(int input, int expected)
+    {
+        MathEx.GetNextPowerOf2Saturating(input).Should().Be(expected);
+    }
+}
---
+++ PSql.Deploy.private/Utilities/MathEx.cs
@@ -0,0 +1,27 @@
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
+
+namespace PSql.Deploy;
+
+internal static class MathEx
+{
+    internal static int GetNextPowerOf2Saturating(int value)
+    {
+        // https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+        // but saturating at int.MaxValue instead of overflow
+
+        if (value <= 0)
+            return 1;
+
+        value--;
+        value |= value >> 1;
+        value |= value >> 2;
+        value |= value >> 4;
+        value |= value >> 8;
+        value |= value >> 16;
+
+        return value == int.MaxValue
+            ? value         // edge case: avoid overflow
+            : value + 1;    // normal
+    }
+}
