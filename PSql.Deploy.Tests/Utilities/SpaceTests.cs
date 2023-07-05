c059ecbdf104c81898dc8983aae54c206c5901d1
Jeff Sharp <public@sharpjs.net>
2023-07-05 15:44:17 -0700

    Add Space class and tests.

---
+++ PSql.Deploy.Tests/Utilities/SpaceTests.cs
@@ -0,0 +1,35 @@
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
+
+namespace PSql.Deploy;
+
+[TestFixture]
+public class SpaceTests
+{
+    [Test]
+    [TestCase("",      0,     "")]
+    [TestCase("",      4, "    ")]
+    [TestCase("abc",   4,    " ")]
+    [TestCase("abcd",  4,     "")]
+    [TestCase("abcde", 4,     "")]
+    public void Pad(string s, int width, string expected)
+    {
+        var a = Space.Pad(s, width);
+        var b = Space.Pad(s, width);
+
+        a.Should().Be(expected).And.BeSameAs(b); 
+    }
+
+    [Test]
+    [TestCase(-1, "")]
+    [TestCase( 0, "")]
+    [TestCase( 1, " ")]
+    [TestCase(10, "          ")]
+    public void Get(int n, string expected)
+    {
+        var a = Space.Get(n);
+        var b = Space.Get(n);
+
+        a.Should().Be(expected).And.BeSameAs(b); 
+    }
+}
---
+++ PSql.Deploy.private/Utilities/Space.cs
@@ -0,0 +1,34 @@
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
+
+namespace PSql.Deploy;
+
+internal static class Space
+{
+    private static string?[]       _spaces = new string[8];
+    private static readonly object _lock   = new object();
+
+    internal static string Pad(string s, int width)
+    {
+        return Get(width - s.Length);
+    }
+
+    internal static string Get(int n)
+    {
+        if (n <= 0)
+            return string.Empty;
+
+        var index = n - 1;
+
+        if (index < _spaces.Length && _spaces[index] is { } space)
+            return space;
+
+        lock (_lock)
+        {
+            if (index >= _spaces.Length)
+                Array.Resize(ref _spaces, MathEx.GetNextPowerOf2Saturating(n));
+
+            return _spaces[index] ??= new(' ', n);
+        }
+    }
+}
