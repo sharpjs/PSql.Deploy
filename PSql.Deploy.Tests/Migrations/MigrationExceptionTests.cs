d2f345a4e05b5c7c85cf8bd3d6f3ac730654efe3
Jeff Sharp <public@sharpjs.net>
2023-07-20 16:41:01 -0700

    Tests for MigrationException.

---
+++ PSql.Deploy.Tests/Migrations/MigrationExceptionTests.cs
@@ -0,0 +1,10 @@
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
+
+namespace PSql.Deploy.Migrations;
+
+[TestFixture]
+public class MigrationExceptionTests : ExceptionTests<MigrationException>
+{
+    // Tests defined by base class
+}
