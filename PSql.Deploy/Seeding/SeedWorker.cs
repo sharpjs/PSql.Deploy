ec44e78670d0dfefcd35f62fdc5589fd9a2144a5
Jeff Sharp <public@sharpjs.net>
2023-07-24 10:08:26 -0700

    Subatomix.PowerShell.TaskHost -> 1.1.0-rc.1

--- PSql.Deploy/Seeding/SeedWorker.cs
+++ PSql.Deploy/Seeding/SeedWorker.cs
@@ -1,20 +1,6 @@
-/*
-    Copyright 2023 Subatomix Research Inc.
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
 
-    Permission to use, copy, modify, and distribute this software for any
-    purpose with or without fee is hereby granted, provided that the above
-    copyright notice and this permission notice appear in all copies.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
-
-using System.Management.Automation;
 using System.Management.Automation.Host;
 using DependencyQueue;
 using Subatomix.PowerShell.TaskHost;
@@ -31,7 +17,7 @@ internal class SeedWorker
     internal SeedWorker(string script, PSHost host)
     {
         _script      = script;
-        _hostFactory = new TaskHostFactory(host);
+        _hostFactory = new TaskHostFactory(host, withElapsed: true);
     }
 
     internal void WorkerMain(QueueContext context)
