cb54cc3ff6f4c027300712ca1d3493330b254624
Jeff Sharp <public@sharpjs.net>
2023-08-04 16:13:11 -0700

    No longer need $DefaultPlanPath.

--- PSql.Deploy/PSql.Deploy.psm1
+++ PSql.Deploy/PSql.Deploy.psm1
@@ -12,10 +12,7 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 3.0
 
 # Enable @Try shorthand to ignore errors
-$Try = @{ ErrorAction = "SilentlyContinue" }
-
-# Default path of directory in which to save migration plans
-$DefaultPlanPath = ".migration-plan"
+$Try = @{ ErrorAction = "Ignore" }
 
 # Read all .ps1 files
 Join-Path $PSScriptRoot Commands *.ps1 -Resolve | ForEach-Object { . $_ }
