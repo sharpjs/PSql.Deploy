ed1b48bdb5ae4fffe4d2fe264082d1ae3a50c9c7
Jeff Sharp <public@sharpjs.net>
2023-12-25 12:11:34 -0800

    Prepare for eventual PS 7.4 / .NET 8 by conditionalizing serializability.

--- PSql.Deploy.Core/Migrations/MigrationException.cs
+++ PSql.Deploy.Core/Migrations/MigrationException.cs
@@ -1,14 +1,18 @@
 // Copyright 2023 Subatomix Research Inc.
 // SPDX-License-Identifier: ISC
 
+#if !NET8_0_OR_GREATER
 using System.Runtime.Serialization;
+#endif
 
 namespace PSql.Deploy.Migrations;
 
 /// <summary>
 ///   Represents an error that occurred during database schema migration.
 /// </summary>
+#if !NET8_0_OR_GREATER
 [Serializable]
+#endif
 public class MigrationException : Exception
 {
     /// <inheritdoc/>
@@ -26,8 +30,10 @@ public class MigrationException : Exception
         : base(message, innerException)
     { }
 
+#if !NET8_0_OR_GREATER
     /// <inheritdoc/>
     protected MigrationException(SerializationInfo info, StreamingContext context)
         : base(info, context)
     { }
+#endif
 }
