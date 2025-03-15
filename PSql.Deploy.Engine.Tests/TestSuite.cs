32e825c8358193eb26ed635dc51307683b53e89e
Jeff Sharp <public@sharpjs.net>
2025-03-15 11:37:26 -0700

    Add test suite for new project.

---
+++ PSql.Deploy.Engine.Tests/TestSuite.cs
@@ -0,0 +1,6 @@
+// Copyright Subatomix Research Inc.
+// SPDX-License-Identifier: MIT
+
+[assembly: Parallelizable(ParallelScope.All)]
+[assembly: FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+[assembly: SetCulture("en-US")]
