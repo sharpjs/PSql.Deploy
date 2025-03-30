fee2cd120f0bee46d74547a77fe71fd7250e84f0
Jeff Sharp <public@sharpjs.net>
2025-03-30 10:01:06 -0700

    Add Space class from existing code, plus documentation.

---
+++ PSql.Deploy.Engine.Tests/Utilities/SpaceTests.cs
@@ -0,0 +1,54 @@
+// Copyright Subatomix Research Inc.
+// SPDX-License-Identifier: MIT
+
+namespace PSql.Deploy;
+
+[TestFixture]
+public class SpaceTests
+{
+    [Test]
+    [TestCase("",      0,     "")]
+    [TestCase("",      4, "    ")]
+    [TestCase("abc",   4,    " ")]
+    [TestCase("abcd",  4,     "")]
+    [TestCase("abcde", 4,     "")]
+    public void Pad(string s, int width, string expected)
+    {
+        var a = Space.Pad(s, width);
+        var b = Space.Pad(s, width);
+
+        a.ShouldBe(expected);
+        a.ShouldBeSameAs(b);
+    }
+
+    [Test]
+    [TestCase(-1, "")]
+    [TestCase( 0, "")]
+    [TestCase( 1, " ")]
+    [TestCase(10, "          ")]
+    public void Get(int n, string expected)
+    {
+        var a = Space.Get(n);
+        var b = Space.Get(n);
+
+        a.ShouldBe(expected);
+        a.ShouldBeSameAs(b);
+    }
+
+    [Test]
+    [TestCase(int.MinValue, "",     "")]
+    [TestCase(-1,           "",     "")]
+    [TestCase( 0,           "",     "")]
+    [TestCase( 1,           " ",    "")]
+    [TestCase( 2,           " ",   " ")]
+    [TestCase( 3,           "  ",  " ")]
+    [TestCase( 4,           "  ", "  ")]
+    public void GetCentering(int n, string expectedLeft, string expectedRight)
+    {
+        var a = Space.GetCentering(n);
+        var b = Space.GetCentering(n);
+
+        a.ShouldBe((expectedLeft, expectedRight));
+        a.ShouldBe(b);
+    }
+}
