7c5060961357b4b94d79e85f177f67db84b2f39d
Jeff Sharp <public@sharpjs.net>
2025-03-30 10:30:50 -0700

    Add, document, and test string extensions.

---
+++ PSql.Deploy.Engine.Tests/Utilities/StringExtensionsTests.cs
@@ -0,0 +1,35 @@
+// Copyright Subatomix Research Inc.
+// SPDX-License-Identifier: MIT
+
+namespace PSql.Deploy;
+
+[TestFixture]
+public class StringExtensionsTests
+{
+    [Test]
+    [TestCase(null, true )]
+    [TestCase("",   true )]
+    [TestCase("a",  false)]
+    public void IsNullOrEmpty(string? s, bool expected)
+    {
+        s.IsNullOrEmpty().ShouldBe(expected);
+    }
+
+    [Test]
+    [TestCase(null, null)]
+    [TestCase("",   null)]
+    [TestCase(" ",  " " )]
+    [TestCase("a",  "a" )]
+    public void NullIfEmpty(string? s, string? expected)
+    {
+        s.NullIfEmpty().ShouldBe(expected);
+    }
+
+    [Test]
+    [TestCase("a", "a" )]
+    [TestCase("'", "''")]
+    public void EscapeForSqlString(string s, string expected)
+    {
+        s.EscapeForSqlString().ShouldBe(expected);
+    }
+}
