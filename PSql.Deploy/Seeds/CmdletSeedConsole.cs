1ef7d4ba70e6c224845e12621c4227774ce5b75f
Jeff Sharp <public@sharpjs.net>
2025-07-27 17:35:04 -0700

    Implement CmdletSeedConsole.

--- PSql.Deploy/Seeds/CmdletSeedConsole.cs
+++ PSql.Deploy/Seeds/CmdletSeedConsole.cs
@@ -3,26 +3,47 @@
 
 namespace PSql.Deploy.Seeds;
 
+/// <summary>
+///   An adapter that enables a PowerShell cmdlet to function as a user
+///   interface to report the progress of content seed application.
+/// </summary>
 internal class CmdletSeedConsole : S.ISeedConsole
 {
     private readonly ICmdlet _cmdlet;
-    private readonly string  _logPath;
+    private readonly string? _logPath;
+    private readonly object  _lock;
 
-    public CmdletSeedConsole(ICmdlet cmdlet, string logPath)
+    /// <summary>
+    ///   Initializes a new <see cref="CmdletSeedConsole"/> instance.
+    /// </summary>
+    /// <param name="cmdlet">
+    ///   The PowerShell cmdlet to adapt.
+    /// </param>
+    /// <param name="logPath">
+    ///   The path of directory in which to store per-phase, per-target log
+    ///   files, or <see langword="null"/> to disable logging.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    ///   <paramref name="cmdlet"/>  is <see langword="null"/>.
+    /// </exception>
+    public CmdletSeedConsole(ICmdlet cmdlet, string? logPath)
     {
-        if (cmdlet is null)
-            throw new ArgumentNullException(nameof(cmdlet));
-        if (logPath is null)
-            throw new ArgumentNullException(nameof(logPath));
+        ArgumentNullException.ThrowIfNull(cmdlet);
 
         _cmdlet  = cmdlet;
         _logPath = logPath;
+        _lock    = new();
 
-        Directory.CreateDirectory(_logPath);
+        if (logPath is not null)
+            Directory.CreateDirectory(logPath);
     }
 
+    /// <inheritdoc/>
     public TextWriter CreateLog(S.ISeedApplication info)
     {
+        if (_logPath is not { } logPath)
+            return TextWriter.Null;
+
         var target = info.Target.FullDisplayName;
         var seed   = info.Seed.Seed.Name;
 
@@ -31,19 +52,63 @@ internal class CmdletSeedConsole : S.ISeedConsole
         return new StreamWriter(Path.Combine(_logPath, fileName));
     }
 
-    public void ReportApplied(S.ISeedApplication info, int count, TimeSpan duration, E.TargetDisposition disposition)
-    {
-    }
-
-    public void ReportApplying(S.ISeedApplication info, string moduleName)
-    {
-    }
-
-    public void ReportProblem(S.ISeedApplication info, string message)
-    {
-    }
-
+    /// <inheritdoc/>
     public void ReportStarting(S.ISeedApplication info)
     {
+        lock (_lock)
+        {
+            WriteHeader(info);
+
+            _cmdlet.WriteHost("Starting");
+        }
+    }
+
+    /// <inheritdoc/>
+    public void ReportApplying(S.ISeedApplication info, string moduleName)
+    {
+        lock (_lock)
+        {
+            WriteHeader(info);
+
+            _cmdlet.WriteHost(string.Format(
+                "Applying {0}",
+                moduleName
+            ));
+        }
+    }
+
+    /// <inheritdoc/>
+    public void ReportApplied(S.ISeedApplication info, int count, TimeSpan duration, E.TargetDisposition disposition)
+    {
+        lock (_lock)
+        {
+            WriteHeader(info);
+
+            _cmdlet.WriteHost(string.Format(
+                "Applied {0} module(s) in {1:N3} second(s){2}",
+                count,
+                duration.TotalSeconds,
+                disposition.ToMarker()
+            ));
+        }
+    }
+
+    /// <inheritdoc/>
+    public void ReportProblem(S.ISeedApplication info, string message)
+    {
+        lock (_lock)
+        {
+            WriteHeader(info, ConsoleColor.Yellow);
+
+            _cmdlet.WriteHost(message, foregroundColor: ConsoleColor.Yellow);
+        }
+    }
+
+    private void WriteHeader(S.ISeedApplication info, ConsoleColor color = ConsoleColor.Blue)
+    {
+        var seed     = info.Seed.Seed.Name;
+        var database = info.Target.DatabaseDisplayName;
+
+        _cmdlet.WriteHost($"[{seed}] [{database}] ", newLine: false, color);
     }
 }
