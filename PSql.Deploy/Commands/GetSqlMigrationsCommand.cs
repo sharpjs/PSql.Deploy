505ee7d159cfaf948874c16a03531e644262061f
Jeff Sharp <public@sharpjs.net>
2025-08-23 06:12:25 -0500

    Add -InputObject parameter for smart pipelilne processing.

--- PSql.Deploy/Commands/GetSqlMigrationsCommand.cs
+++ PSql.Deploy/Commands/GetSqlMigrationsCommand.cs
@@ -14,21 +14,33 @@ using AllowNullAttribute = System.Management.Automation.AllowNullAttribute;
 /// <remarks>
 ///   Lists database schema migrations.
 /// </remarks>
-[Cmdlet(VerbsCommon.Get, "SqlMigrations", DefaultParameterSetName = "Path")]
+[Cmdlet(VerbsCommon.Get, "SqlMigrations", DefaultParameterSetName = nameof(InputObject))]
 [OutputType(typeof(Migration))]
 public sealed class GetSqlMigrationsCommand : AsyncPSCmdlet
 {
+    /// <summary>
+    ///   <b>-InputObject:</b> Any object accepted by the <c>-Path</c> or the
+    ///   <c>-Target</c> parameter.  The cmdlet determines which parameter to
+    ///   use.
+    /// </summary>
+    /// <remarks>
+    ///   string | SqlContext | SqlTargetDatabase
+    /// </remarks>
+    [Parameter(
+        ParameterSetName  = nameof(InputObject),
+        Position          = 0,
+        Mandatory         = false,
+        ValueFromPipeline = true
+    )]
+    [ValidateNotNullOrEmpty]
+    public object? InputObject { get; set; }
+
     /// <summary>
     ///   <b>-Path:</b> Path to a directory containing database source code in
     ///   the layout expected by PSql.Deploy.  The default is the current
     ///   directory.
     /// </summary>
-    [Parameter(
-        ParameterSetName  = "Path",
-        Mandatory         = false,
-        Position          = 0,
-        ValueFromPipeline = true
-    )]
+    [Parameter(ParameterSetName = nameof(Path), Mandatory = true)]
     [Alias("PSPath")]
     [ValidateNotNullOrEmpty]
     public string? Path { get; set; }
@@ -47,12 +59,7 @@ public sealed class GetSqlMigrationsCommand : AsyncPSCmdlet
     /// <remarks>
     ///   string | SqlContext | SqlTargetDatabase
     /// </remarks>
-    [Parameter(
-        ParameterSetName  = "Target",
-        Mandatory         = true,
-        Position          = 0,
-        ValueFromPipeline = true
-    )]
+    [Parameter(ParameterSetName = nameof(Target), Mandatory = true)]
     [ValidateNotNullOrEmpty]
     public SqlTargetDatabase? Target { get; set; }
 
@@ -61,7 +68,8 @@ public sealed class GetSqlMigrationsCommand : AsyncPSCmdlet
     ///   <see langword="null"/> to to return all migrations registered on the
     ///   target database.  The default is <see langword="null"/>.
     /// </summary>
-    [Parameter(ParameterSetName  = "Target")]
+    [Parameter(ParameterSetName = nameof(InputObject))]
+    [Parameter(ParameterSetName = nameof(Target))]
     [AllowNull, AllowEmptyString]
     public string? MinimumName { get; set; }
 
@@ -73,11 +81,9 @@ public sealed class GetSqlMigrationsCommand : AsyncPSCmdlet
 
     private async Task ProcessRecordAsync()
     {
-        var migrations = ParameterSetName switch
-        {
-            "Path" => GetMigrations(Path),
-            _      => await GetMigrationsAsync(Target!),
-        };
+        var migrations = IsForTarget()
+            ? await GetMigrationsAsync(Target)
+            : GetMigrations(Path);
 
 #if INCLUDE_CONTENT
         if (IncludeContent.IsPresent)
@@ -86,6 +92,38 @@ public sealed class GetSqlMigrationsCommand : AsyncPSCmdlet
 
         foreach (var migration in migrations)
             WriteObject(new Migration(migration));
+    }
+
+    [MemberNotNullWhen(true, nameof(Target))]
+    private bool IsForTarget()
+    {
+        while (InputObject is PSObject psObject)
+            InputObject = psObject.BaseObject;
+
+        switch (ParameterSetName)
+        {
+            case nameof(Path):
+                return false;
+
+            case nameof(Target):
+                Assume.NotNull(Target);
+                return true;
+
+            case nameof(InputObject) when InputObject is null:
+                return false;
+
+            case nameof(InputObject) when InputObject is string path && !path.Contains(';'):
+                Path = path;
+                return false;
+
+            case nameof(InputObject) when InputObject is SqlTargetDatabase target:
+                Target = target;
+                return true;
+
+            default:
+                Target = new SqlTargetDatabase(InputObject!);
+                return true;
+        }
     }
 
     private IReadOnlyList<M.Migration> GetMigrations(string? path)
